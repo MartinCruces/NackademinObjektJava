39ae28e253c94eb2a86697a8e6d82cd54f560990
Martin Cruces <martin76e@yahoo.com>
2022-10-13 12:27:24 +0200

    Circle finished

--- Test/Sprint2/Uppgift1/CircleTest.java
+++ Test/Sprint2/Uppgift1/CircleTest.java
@@ -1,7 +1,25 @@
 package Sprint2.Uppgift1;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
 
+    Circle c1 = new Circle(4);
+
+    @Test
+
+    public void testCircleArea(){
+
+        assertTrue(c1.getArea() - 50.265 > 0);
+        assertTrue(c1.getArea() - 50.266 < 0);
+        System.out.println(c1.getArea());
+    }
+    @Test
+    public void testCircleCircumference(){
+
+        assertTrue(c1.getCircumference() - 25.132 > 0);
+        assertTrue(c1.getCircumference() - 25.133 < 0);
+    }
 }
--- src/Sprint2/Uppgift1/Circle.java
+++ src/Sprint2/Uppgift1/Circle.java
@@ -1,5 +1,20 @@
 package Sprint2.Uppgift1;
 
-public class Circle {
+public class Circle implements Figure {
+    protected double radius;
 
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+
+    @Override
+    public double getArea() {
+        return Math.PI*(radius*radius);
+    }
+
+    @Override
+    public double getCircumference() {
+        return Math.PI*(radius + radius);
+    }
 }
